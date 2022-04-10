895dee039f7e23c6cd4b77ca6ed44dd5ba936a0d
linuxssd <david.coronel@outlook.com>
2022-04-10 09:30:37 -0500

    improved weather details UI

--- app/src/main/java/com/demo/metaweather/utils/DateFormatter.kt
+++ app/src/main/java/com/demo/metaweather/utils/DateFormatter.kt
@@ -9,7 +9,7 @@ interface DateFormatter {
     fun getLongDateFromString(date: String): String
 }
 
-const val LONG_DATE_PATTERN = "EEEE, MMMM dd, h:mm:ss aa"
+const val LONG_DATE_PATTERN = "EEEE, MMMM dd"
 const val SHORT_DATE_PATTERN = "yyyy-MM-dd"
 
 class WeatherDatesFormatter @Inject constructor() : DateFormatter {
